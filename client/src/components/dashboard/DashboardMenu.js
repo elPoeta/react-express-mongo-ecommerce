47b7c771b7edddf2ce846ce546d868cf0f704cda
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-03-25 19:24:51 -0300

    add my account link to dashboard

--- client/src/components/dashboard/DashboardMenu.js
+++ client/src/components/dashboard/DashboardMenu.js
@@ -9,15 +9,15 @@ const DashboardMenu = () =>
             <div>
                 <i className="fas fa-search"></i><input type="search" />
             </div>
-            <ul>
+            <ul className='dashboard-menu'>
                 <li><Link to='/admin/add-category'><i className='fas fa-plus' />Add Category</Link></li>
                 <li><Link to='/admin/edit-category'><i className='fas fa-edit' />Edit Category</Link></li>
                 <li><Link to='/admin/add-product'><i className='fas fa-plus' />Add Product</Link></li>
                 <li><Link to='/admin/edit-product'><i className='fas fa-edit' />Edit Product</Link></li>
+                <li><Link to='/my-account'><i className='fas fa-user' />My Account</Link></li>
             </ul>
 
         </div>
     )
-
 
 export default DashboardMenu
