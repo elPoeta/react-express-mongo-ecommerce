27db7d4bbb550b02f6cdef7ca5e46b0315e9a22b
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-03-26 14:20:20 -0300

    start checkout component

--- client/src/components/shopping-cart/Checkout.js
+++ client/src/components/shopping-cart/Checkout.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import UserRoute from '../../HOC/UserRoute';
+import { getCustomer } from '../../actions/customerAction';
+import isEmpty from '../../utils/isEmpty';
 
 class Checkout extends Component {
+  async componentDidMount() {
+    await this.props.getCustomer();
+  }
   render() {
+    const { customer, loading } = this.props.customer;
+    if (loading || isEmpty(customer)) {
+      return <Redirect to='/createcustomer' />
+    } else if (!isEmpty(customer) && isEmpty(customer.address)) {
+      return <Redirect to='/addaddress' />
+    }
     return <div>Checkout</div>;
   }
 }
-
-export default Checkout;
+const mapStateToProps = state => ({
+  auth: state.auth,
+  customer: state.customer
+});
+export default connect(mapStateToProps, { getCustomer })(UserRoute(Checkout));
