b6f3ecbb31ce5fb1d75f2f2ca0b44703279190d3
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-04-01 22:50:47 -0300

    fix port heroku deploy

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const { PORT } = require("./config/keys");
+//const { PORT } = require("./config/keys");
 const app = express();
 
 require("./startUp/db")();
 require("./startUp/parser")(app);
 require("./startUp/routes")(app);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, err => {
   if (err) {
