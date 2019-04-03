62eb43b4438b1530e427f28d3630046199f3b932
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-04-03 19:32:16 -0300

    comment redux dev tool

--- client/src/store.js
+++ client/src/store.js
@@ -10,7 +10,7 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+      //  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 );
 
