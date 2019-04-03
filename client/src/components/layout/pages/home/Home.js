14bc1016f3858b264f52f8dbfd0d10b8827a9d41
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-04-03 09:34:55 -0300

    styles for home component

--- client/src/components/layout/pages/home/Home.js
+++ client/src/components/layout/pages/home/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
     }
     render() {
         return (
-            <div>
+            <div className='home-container'>
                 <section id="showcase" className="grid">
                     <div className="bg-image"></div>
                     <div className="content-wrap">
