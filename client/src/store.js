3d93d1174bcd2fc5ff2eaecc6475d0a7266d2a9a
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-04-03 19:46:55 -0300

    fix comment redux dev tool

--- client/src/store.js
+++ client/src/store.js
@@ -8,11 +8,7 @@ const initialState = {};
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-      //  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
-);
+    compose( applyMiddleware(...middleware) ) );
 
 export default store;
 
